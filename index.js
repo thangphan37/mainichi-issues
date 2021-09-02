6aa0bacb66f31f416e7e8a2cc110c8ec551f9475
ThangPhan <thang.phan@kinto-mobility.biz>
2021-09-02 09:23:59 +0900

    format code

--- index.js
+++ index.js
@@ -40,22 +40,25 @@ const SUB_ISSUE_TEMPLATE = `
 `
 
 function pbcopy(data) {
-	const proc = require('child_process').spawn('pbcopy');
-	proc.stdin.write(data);
-	proc.stdin.end();
+  const proc = require('child_process').spawn('pbcopy')
+  proc.stdin.write(data)
+  proc.stdin.end()
 }
 
 async function makeIssues() {
-	const { numberOfIssue } = await inquirer.prompt({
-		type: 'number',
-		name: 'numberOfIssue',
-		message: 'How many issues do you have today?',
-	})
-	const hr = Array.from({ length: 50 }, () => '-').join('')
-	const subIssues = Array.from({ length: 3 }, () => SUB_ISSUE_TEMPLATE).join('')
-	const issues = Array.from({ length: numberOfIssue }, () => ISSUE_TEMPLATE + subIssues).join(`\n// ${hr}\n`)
+  const {numberOfIssue} = await inquirer.prompt({
+    type: 'number',
+    name: 'numberOfIssue',
+    message: 'How many issues do you have today?',
+  })
+  const hr = Array.from({length: 50}, () => '-').join('')
+  const subIssues = Array.from({length: 3}, () => SUB_ISSUE_TEMPLATE).join('')
+  const issues = Array.from(
+    {length: numberOfIssue},
+    () => ISSUE_TEMPLATE + subIssues,
+  ).join(`\n// ${hr}\n`)
 
-	pbcopy(issues)
+  pbcopy(issues)
 }
 
 makeIssues()
