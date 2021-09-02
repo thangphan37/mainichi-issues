c970c63d75772d1fd5a42543e9eb46f8b8f5d554
ThangPhan <thang.phan@kinto-mobility.biz>
2021-09-02 09:07:56 +0900

    init app

---
+++ README.md
@@ -0,0 +1,4 @@
+Make your issues every day through the method that I have learned from a Japanese person.
+
+It's `Sky-Rain-Umbrella`.
+
---
+++ index.js
@@ -0,0 +1,61 @@
+const inquirer = require('inquirer')
+const ISSUE_TEMPLATE = `
+/**
+ * Issue: TODO
+ * 🤬sub-issue:
+ * 🤬sub-issue:
+ * 🤬sub-issue:
+ */
+`
+const SUB_ISSUE_TEMPLATE = `
+/**
+ * Sky(sub-issue):
+ * 
+ * Rain(I have to consider):
+
+ * 👉
+ * Umbrella(I’m going to do):
+	* ✌️
+ 	* ✌️
+ 	* ✌️
+
+ * 👉
+ * Umbrella(I’m going to do):
+	* ✌️
+ 	* ✌️
+ 	* ✌️
+
+ * 👉
+ * Umbrella(I’m going to do):
+	* ✌️
+ 	* ✌️
+ 	* ✌️
+ 
+ * Why-What-How-Where
+ *
+ * ✌️Why A?
+ * ✌️Why B?
+ * ✌️Why C?
+ */
+`
+
+function pbcopy(data) {
+	const proc = require('child_process').spawn('pbcopy');
+	proc.stdin.write(data);
+	proc.stdin.end();
+}
+
+async function makeIssues() {
+	const { numberOfIssue } = await inquirer.prompt({
+		type: 'number',
+		name: 'numberOfIssue',
+		message: 'How many issues do you have today?',
+	})
+	const hr = Array.from({ length: 50 }, () => '-').join('')
+	const subIssues = Array.from({ length: 3 }, () => SUB_ISSUE_TEMPLATE).join('')
+	const issues = Array.from({ length: numberOfIssue }, () => ISSUE_TEMPLATE + subIssues).join(`\n// ${hr}\n`)
+
+	pbcopy(issues)
+}
+
+makeIssues()
